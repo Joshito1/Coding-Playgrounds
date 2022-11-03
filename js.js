e7a7179788798225d239c3a0d6ea745cdbd2ff22
Joshi <81931812+Joshito1@users.noreply.github.com>
2022-11-02 22:15:26 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function bg_on() {
+  document.body.style.backgroundColor = "white";
+}
+
+function bg_off() {
+  document.body.style.backgroundColor = "black";
+}
+
+function lightbulbon() {
+  document.getElementById("myimage").src = "images/pic_bulbon.gif";
+}
+
+function lightbulboff() {
+  document.getElementById("myimage").src = "images/pic_bulboff.gif";
+}
