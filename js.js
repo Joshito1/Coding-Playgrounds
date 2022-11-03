82bdf33d8f2ba4ddfd9c916b88e66cc0af2bbaef
Joshi <81931812+Joshito1@users.noreply.github.com>
2022-11-03 10:16:08 -0400

    Add files via upload

--- js.js
+++ js.js
@@ -1,15 +1,35 @@
 function bg_on() {
-  document.body.style.backgroundColor = "white";
+  document.body.style.backgroundColor = "initial";
 }
 
 function bg_off() {
   document.body.style.backgroundColor = "black";
 }
 
-function lightbulbon() {
+function lightbulb_on() {
   document.getElementById("myimage").src = "images/pic_bulbon.gif";
 }
 
-function lightbulboff() {
+function lightbulb_off() {
   document.getElementById("myimage").src = "images/pic_bulboff.gif";
+}
+
+function big_size() {
+  document.getElementById("px").style.fontSize = "35px";
+}
+
+function normal_size() {
+  document.getElementById("px").style.fontSize = "initial";
+}
+
+function hide_Elem() {
+  document.getElementById("hide/show").style.display = "none";
+}
+
+function show_Elem() {
+  document.getElementById("hide/show").style.display = "block";
+}
+
+function show_hidden_Elem() {
+  document.getElementById("hidden_txt").style.display = "block";
 }
